f681e60a6ffa8c1f937dc8970facd3294dcdd0b3
Prof Evil <user@users-MacBook.local>
2017-12-13 16:19:33 +0100

    updates

--- navy/application/views/evaluation.php
+++ navy/application/views/evaluation.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <!--Right content-->
   <div class="col-md-9 right-content">
-
+    <?php //$this->load->view('test_view'); ?>
     <!--Portfolio Tab-->
     <section id="portfolio" class="bgWhite ofsInBottom">
       <!--Portfolio -->
@@ -41,10 +41,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="block-filter tCenter">
                   <ul id="category" class="filter nav nav-tabs">
                     <li><a href="#tab-1" data-toggle="tab" class="">officer particulars</a></li>
-                    <li><a href="#tab-2" data-toggle="tab" class="">Web design</a></li>
-                    <li><a href="#tab-3" data-toggle="tab" class="">Graphic design</a></li>
-                    <li><a href="#tab-4" data-toggle="tab" class="">Motion Graphic</a></li>
+                    <li><a href="#tab-2" data-toggle="tab" class="">Evaluation Data</a></li>
+                    <li><a href="#tab-3" data-toggle="tab" class="">Other Data 1</a></li>
+                    <li><a href="#tab-4" data-toggle="tab" class="">Other Data 2</a></li>
                   </ul>
+
+
 
                 </div>
 
@@ -52,6 +54,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               </div>
               <!--End row-->
 
+                <div class="row tab-content">
+
+                  <!--TAB 1-->
+                  <div class="tab-pane fade in active" id="tab-1">
+                    <div class="row">
+                      <div class="divider-m tCenter margTSSmall margBSmall clearfix">
+                        <div class="col-md-12">
+                          <!-- progressbar -->
+                          <ul id="progressbar">
+                            <li class="active">Personal Details</li>
+                            <li>Appointments</li>
+                            <li>Courses Attended</li>
+                          </ul>
+                          <!-- fieldsets -->
+                        </div>
+                      </div>
+                    </div>
+                    <?php
+                    $attributes = array('id'=> 'msform');
+                    echo form_open('', $attributes) ?>
+
+                    <!-- fieldsets -->
+                    <fieldset>
+                        <h2 class="fs-title">Personal Details</h2>
+                        <h3 class="fs-subtitle">Tell us something more about you</h3>
+                        <input type="text" name="fname" placeholder="First Name"/>
+                        <input type="text" name="lname" placeholder="Last Name"/>
+                        <input type="text" name="phone" placeholder="Phone"/>
+                        <input type="button" name="next" class="next action-button" value="Next"/>
+                    </fieldset>
+                    <div class="form-group">
+                      <label for="current_appointment">Current Appointment:</label>
+                      <div class="col-md-8">
+                        <input type="text" class="form-control" name="current_appointment" id="current_appointment">
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <!--TAB 2-->
+                  <div class="tab-pane fade" id="tab-2">
+                      <h3><i class="fa hearseicon-"></i> Evaluation</h3>
+                      <div class="col-md-12">
+                          Lorem ipsum dolot
+                      </div>
+                      Lorem ipsum dolot
+
+
+                  </div>
+
+                </div>
 
 
             </div>
@@ -64,6 +117,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
     </section>
+
   </div>
 
 
@@ -73,13 +127,103 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $(function(){
   $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
       localStorage.setItem('activeTab', $(e.target).attr('href'));
-      $(this).addClass('active');
   });
+  $('a[data-toggle="tab"]').click(function(){
+    $('a[data-toggle="tab"]').removeClass('active');
+    $(this).addClass('active');
+    $(this).focus();
+
+  });
+
   var activeTab = localStorage.getItem('activeTab');
   if(activeTab){
       $('#category a[href="' + activeTab + '"]').tab('show');
       $('#category a[href="' + activeTab + '"]').addClass('active');
   }
+
+
+
+  //jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+	//show the next fieldset
+	next_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		},
+		duration: 800,
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		},
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+  $(".previous").click(function(){
+  	if(animating) return false;
+  	animating = true;
+
+  	current_fs = $(this).parent();
+  	previous_fs = $(this).parent().prev();
+
+  	//de-activate current step on progressbar
+  	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+  	//show the previous fieldset
+  	previous_fs.show();
+  	//hide the current fieldset with style
+  	current_fs.animate({opacity: 0}, {
+  		step: function(now, mx) {
+  			//as the opacity of current_fs reduces to 0 - stored in "now"
+  			//1. scale previous_fs from 80% to 100%
+  			scale = 0.8 + (1 - now) * 0.2;
+  			//2. take current_fs to the right(50%) - from 0%
+  			left = ((1-now) * 50)+"%";
+  			//3. increase opacity of previous_fs to 1 as it moves in
+  			opacity = 1 - now;
+  			current_fs.css({'left': left});
+  			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+  		},
+  		duration: 800,
+  		complete: function(){
+  			current_fs.hide();
+  			animating = false;
+  		},
+  		//this comes from the custom easing plugin
+  		easing: 'easeInOutBack'
+  	});
+  });
+
+  $(".submit").click(function(){
+  	return false;
+  })
 });
 
 
